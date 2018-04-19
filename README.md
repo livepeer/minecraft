b24370dedf96fc86917b4b505f23779df5074471
jozanza <hello@jsavary.com>
2018-04-19 17:48:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Minecraft
+
+## Getting Started
+
+You first, encode the text dump of eth addresses in `accounts.txt`
+
+```bash
+node encode accounts.txt accounts.bin
+```
+
+Then you can start the dapp
+
+```bash
+yarn start
+```
+
+If you visit in your browser, you'll see a hotpink loading bar. That is index.js requesting the account address binary and parsing it into ETH address strings.
+
+That's all I got for now.
+
---
+++ utils.js
@@ -0,0 +1,40 @@
+const exp0rt = f => (exports[f.name] = f);
+
+exp0rt(toHexString);
+exp0rt(uint32ArrayToAddrs);
+exp0rt(addrToUint32Array);
+exp0rt(toArrayBuffer);
+
+function toHexString(x) {
+  return x.toString(16).padStart(8, "0");
+}
+
+function uint32ArrayToAddrs(xs) {
+  const offset = 5;
+  let i = 0;
+  let addr = "";
+  for (const x of xs) {
+    addr += toHexString(x);
+    if (++i % 5 === 0) addr += "\n";
+  }
+  return addr;
+}
+
+function addrToUint32Array(addr) {
+  const xs = new Uint32Array(5);
+  const hex = addr.match(/.{8}/g);
+  // console.log(hex)
+  for (let i = 0; i < 5; i++) {
+    xs[i] = parseInt(hex[i], 16);
+  }
+  return xs;
+}
+
+function toArrayBuffer(buffer) {
+  const ab = new ArrayBuffer(buffer.length);
+  const view = new Uint8Array(ab);
+  for (let i = 0; i < buffer.length; ++i) {
+    view[i] = buffer[i];
+  }
+  return ab;
+}
